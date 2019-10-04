a9392f45286652d901145dfb13807b0278c9b282
Sho <sho.ohata@berkeley.edu>
2019-10-04 19:47:22 -0400

    initial test models

---
+++ helper_scripts/ml_helpers.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+from pandas.api.types import is_string_dtype, is_numeric_dtype
+import re
+import math
+
+def add_datepart(df, fldname, drop=True, time=False):
+    """add_datepart converts a column of df from a datetime64 to many columns containing
+    the information from the date. This applies changes inplace.
+
+    Parameters:
+    -----------
+    df: A pandas data frame. df gain several new columns.
+    fldname: A string that is the name of the date column you wish to expand.
+        If it is not a datetime64 series, it will be converted to one with pd.to_datetime.
+    drop: If true then the original date column will be removed.
+    time: If true time features: Hour, Minute, Second will be added.
+
+    Examples:
+    ---------
+
+    >>> df = pd.DataFrame({ 'A' : pd.to_datetime(['3/11/2000', '3/12/2000', '3/13/2000'], infer_datetime_format=False) })
+    >>> df
+
+        A
+    0   2000-03-11
+    1   2000-03-12
+    2   2000-03-13
+
+    >>> add_datepart(df, 'A')
+    >>> df
+
+        AYear AMonth AWeek ADay ADayofweek ADayofyear AIs_month_end AIs_month_start AIs_quarter_end AIs_quarter_start AIs_year_end AIs_year_start AElapsed
+    0   2000  3      10    11   5          71         False         False           False           False             False        False          952732800
+    1   2000  3      10    12   6          72         False         False           False           False             False        False          952819200
+    2   2000  3      11    13   0          73         False         False           False           False             False        False          952905600
+    """
+    fld = df[fldname]
+    fld_dtype = fld.dtype
+    if isinstance(fld_dtype, pd.core.dtypes.dtypes.DatetimeTZDtype):
+        fld_dtype = np.datetime64
+
+    if not np.issubdtype(fld_dtype, np.datetime64):
+        df[fldname] = fld = pd.to_datetime(fld, infer_datetime_format=True)
+    targ_pre = re.sub('[Dd]ate$', '', fldname)
+    attr = ['Month', 'Day', 'Dayofweek', 'Is_month_end', 'Is_month_start', 'Is_year_end', 'Is_year_start']
+    if time: attr = attr + ['Hour', 'Minute', 'Second']
+    for n in attr: df[targ_pre + n] = getattr(fld.dt, n.lower())
+    if drop: df.drop(fldname, axis=1, inplace=True)
+
+def train_cats(df):
+    """Change any columns of strings in a panda's dataframe to a column of
+    categorical values. This applies the changes inplace.
+
+    Parameters:
+    -----------
+    df: A pandas dataframe. Any columns of strings will be changed to
+        categorical values.
+
+    Examples:
+    ---------
+
+    >>> df = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['a', 'b', 'a']})
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    note the type of col2 is string
+
+    >>> train_cats(df)
+    >>> df
+
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    now the type of col2 is category
+    """
+    for n,c in df.items():
+        if is_string_dtype(c): df[n] = c.astype('category').cat.as_ordered()
+
+def apply_cats(df, trn):
+    """Changes any columns of strings in df into categorical variables using trn as
+    a template for the category codes.
+    Parameters:
+    -----------
+    df: A pandas dataframe. Any columns of strings will be changed to
+        categorical values. The category codes are determined by trn.
+    trn: A pandas dataframe. When creating a category for df, it looks up the
+        what the category's code were in trn and makes those the category codes
+        for df.
+    Examples:
+    ---------
+    >>> df = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['a', 'b', 'a']})
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+    note the type of col2 is string
+    >>> train_cats(df)
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+    now the type of col2 is category {a : 1, b : 2}
+    >>> df2 = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['b', 'a', 'a']})
+    >>> apply_cats(df2, df)
+           col1 col2
+        0     1    b
+        1     2    a
+        2     3    a
+    now the type of col is category {a : 1, b : 2}
+    """
+    for n,c in df.items():
+        if (n in trn.columns) and (trn[n].dtype.name=='category'):
+            df[n] = c.astype('category').cat.as_ordered()
+            df[n].cat.set_categories(trn[n].cat.categories, ordered=True, inplace=True)
+
+def numericalize(df, col, name, max_n_cat):
+    """ Changes the column col from a categorical type to it's integer codes.
+
+    Parameters:
+    -----------
+    df: A pandas dataframe. df[name] will be filled with the integer codes from
+        col.
+
+    col: The column you wish to change into the categories.
+    name: The column name you wish to insert into df. This column will hold the
+        integer codes.
+
+    max_n_cat: If col has more categories than max_n_cat it will not change the
+        it to its integer codes. If max_n_cat is None, then col will always be
+        converted.
+
+    Examples:
+    ---------
+    >>> df = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['a', 'b', 'a']})
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    note the type of col2 is string
+
+    >>> train_cats(df)
+    >>> df
+
+
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    now the type of col2 is category { a : 1, b : 2}
+
+    >>> numericalize(df, df['col2'], 'col3', None)
+
+       col1 col2 col3
+    0     1    a    1
+    1     2    b    2
+    2     3    a    1
+    """
+    if not is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
+        df[name] = col.cat.codes+1
+
+def rmse(x,y): return math.sqrt(((x-y)**2).mean())
+
+def print_score(m):
+    res = [rmse(m.predict(X_train), y_train), rmse(m.predict(X_valid), y_valid),
+                m.score(X_train, y_train), m.score(X_valid, y_valid)]
+    if hasattr(m, 'oob_score_'): res.append(m.oob_score_)
+    print(res)
